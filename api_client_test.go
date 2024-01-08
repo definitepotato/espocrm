8eaa85f498d023e2b2ca90b17e343fc4ab59ff6f
definitepotato <definitepotato@gmail.com>
2024-01-07 20:06:58 -0500

    task() quick and dirty tests for create() update() read() delete(), refactor later.

--- api_client_test.go
+++ api_client_test.go
@@ -1,11 +1,18 @@
 package espocrm
 
 import (
+	"encoding/json"
 	"os"
 	"testing"
 )
 
-func TestNewApiClient(t *testing.T) {
+func TestApiClient(t *testing.T) {
+	type Entity struct {
+		Id   string
+		Name string
+	}
+	E := &Entity{}
+
 	t.Run("new api client with api key", func(t *testing.T) {
 		url := os.Getenv("GOESPOCRM_URL")
 		apiKey := os.Getenv("GOESPOCRM_APIKEY")
@@ -32,14 +39,107 @@ func TestNewApiClient(t *testing.T) {
 			t.Errorf("got password {%v}, wanted {%v}", *client.config.password, password)
 		}
 	})
+
+	t.Run("create new entity", func(t *testing.T) {
+		url := os.Getenv("GOESPOCRM_URL")
+		apiKey := os.Getenv("GOESPOCRM_APIKEY")
+		entity := os.Getenv("GOESPOCRM_ENTITY")
+		payload := os.Getenv("GOESPOCRM_CREATE_ENTITY_PAYLOAD")
+
+		client := NewApiClient(url, WithApiKeyAuth(apiKey))
+
+		result, err := client.Create(entity, payload)
+		if err != nil {
+			t.Errorf("%s", err)
+		}
+
+		var newEntity map[string]any
+		err = json.Unmarshal(result, &newEntity)
+		if err != nil {
+			t.Errorf("%s", err)
+		}
+
+		if newEntity == nil {
+			t.Errorf("entity is empty, expected new created entity")
+		}
+
+		E.Id = newEntity["id"].(string)
+		E.Name = newEntity["name"].(string)
+	})
+
+	t.Run("update existing entity", func(t *testing.T) {
+		url := os.Getenv("GOESPOCRM_URL")
+		apiKey := os.Getenv("GOESPOCRM_APIKEY")
+		entity := os.Getenv("GOESPOCRM_ENTITY")
+		payload := os.Getenv("GOESPOCRM_UPDATE_ENTITY_PAYLOAD")
+
+		client := NewApiClient(url, WithApiKeyAuth(apiKey))
+
+		result, err := client.Update(entity, E.Id, payload)
+		if err != nil {
+			t.Errorf("%s", err)
+		}
+
+		var newEntity map[string]any
+		err = json.Unmarshal(result, &newEntity)
+		if err != nil {
+			t.Errorf("%s", err)
+		}
+
+		if newEntity == nil {
+			t.Errorf("entity is empty, expected new created entity")
+		}
+
+		var updatePayload map[string]string
+		err = json.Unmarshal([]byte(payload), &updatePayload)
+		if err != nil {
+			t.Errorf("%s", err)
+		}
+
+		if newEntity["name"] != updatePayload["name"] {
+			t.Errorf("expected name {%v}, got name {%v}", updatePayload["name"], newEntity["name"])
+		}
+	})
+
+	t.Run("read updated entity", func(t *testing.T) {
+		url := os.Getenv("GOESPOCRM_URL")
+		apiKey := os.Getenv("GOESPOCRM_APIKEY")
+		entity := os.Getenv("GOESPOCRM_ENTITY")
+
+		client := NewApiClient(url, WithApiKeyAuth(apiKey))
+
+		result, err := client.Read(entity, E.Id)
+		if err != nil {
+			t.Errorf("%s", err)
+		}
+
+		var updatedEntity map[string]any
+		err = json.Unmarshal(result, &updatedEntity)
+		if err != nil {
+			t.Errorf("%s", err)
+		}
+
+		if E.Name == updatedEntity["name"].(string) {
+			t.Errorf("expected entity name to change")
+		}
+	})
+
+	t.Run("delete updated entity", func(t *testing.T) {
+		url := os.Getenv("GOESPOCRM_URL")
+		apiKey := os.Getenv("GOESPOCRM_APIKEY")
+		entity := os.Getenv("GOESPOCRM_ENTITY")
+
+		client := NewApiClient(url, WithApiKeyAuth(apiKey))
+
+		result, err := client.Delete(entity, E.Id)
+		if err != nil {
+			t.Errorf("%s", err)
+		}
+
+		if string(result) != "true" {
+			t.Errorf("expected true, got {%v}", result)
+		}
+	})
 }
 
-// TODO: Test ApiClient.Read()
-
-// TODO: Test ApiClient.Delete()
-
 // TODO: Test ApiClient.List()
-
-// TODO: Test ApiClient.Update()
-
-// TODO: Test ApiClient.Create()
