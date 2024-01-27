977301063bf1e69091c89218ad5bbe0c321f162f
definitepotato <definitepotato@gmail.com>
2024-01-26 21:52:23 -0500

    task() adding ListRelated(), Link() and Unlink(), as well as some doc comments.

--- api_client.go
+++ api_client.go
@@ -50,6 +50,7 @@ func NewApiClient(uri string, opts ...ApiClientOption) *ApiClient {
 	return client
 }
 
+// Read will fetch an `entityType` record based on an `id`.
 func (client *ApiClient) Read(entityType, id string) ([]byte, error) {
 	method := "GET"
 	endpoint := fmt.Sprintf("%s/%s/%s", client.url.String(), entityType, id)
@@ -67,6 +68,7 @@ func (client *ApiClient) Read(entityType, id string) ([]byte, error) {
 	return response, err
 }
 
+// Delete will delete an `entityType` record based on an `id`.
 func (client *ApiClient) Delete(entityType, id string) ([]byte, error) {
 	method := "DELETE"
 	endpoint := fmt.Sprintf("%s/%s/%s", client.url.String(), entityType, id)
@@ -148,6 +150,9 @@ func (client *ApiClient) List(entityType string, params *Parameters) ([]byte, er
 	return response, err
 }
 
+// Update will update an `entityType` record based on an `id`. The `payload` should be a json string
+// based on documentation found in https://docs.espocrm.com/development/api/crud/#update
+// and should contain attributes to update for the given record.
 func (client *ApiClient) Update(entityType, id, payload string) ([]byte, error) {
 	method := "PUT"
 	endpoint := fmt.Sprintf("%s/%s/%s", client.url.String(), entityType, id)
@@ -165,11 +170,139 @@ func (client *ApiClient) Update(entityType, id, payload string) ([]byte, error) 
 	return response, err
 }
 
+// Create will create a new `entityType` record. The `payload` should be a json string
+// based on documentation found in https://docs.espocrm.com/development/api/crud/#create
+// and should contain attributes for the new record.
 func (client *ApiClient) Create(entityType, payload string) ([]byte, error) {
 	method := "POST"
 	endpoint := fmt.Sprintf("%s/%s", client.url.String(), entityType)
 
 	request, err := NewRequestWithBody(client, method, endpoint, json.RawMessage(payload))
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := SendRequest(request)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, err
+}
+
+// ListRelated will fetch all records related to `entityType` based on `id`.
+// The `relatedEntityType` is the related record type, based on documentation
+// found in https://docs.espocrm.com/development/api/relationships/#list-related-records.
+func (client *ApiClient) ListRelated(entityType, id, relatedEntityType string, params *Parameters) ([]byte, error) {
+	method := "GET"
+	client.url.Path += fmt.Sprintf("/%s/%s/%s", entityType, id, relatedEntityType)
+	paramValues := url.Values{}
+
+	if params.MaxSize != nil {
+		maxSize := fmt.Sprintf("%v", *params.MaxSize)
+		paramValues.Add("maxSize", maxSize)
+	}
+
+	if params.OrderBy != nil {
+		paramValues.Add("orderBy", *params.OrderBy)
+	}
+
+	if params.Select != nil {
+		paramValues.Add("select", *params.Select)
+	}
+
+	if params.Order != nil {
+		order := "desc"
+
+		if *params.Order == Ascending {
+			order = "asc"
+		}
+		paramValues.Add("order", order)
+	}
+
+	if params.Offset != nil {
+		offset := fmt.Sprintf("%d", params.Offset)
+		paramValues.Add("offset", offset)
+	}
+
+	if params.ReturnTotal != nil {
+		returnTotal := "false"
+
+		if *params.ReturnTotal {
+			returnTotal = "true"
+		}
+		paramValues.Add("returnTotal", returnTotal)
+	}
+
+	if params.Where != nil {
+		for i := 0; i < len(params.Where); i++ {
+			paramValues.Add("where[%d][type]=%s", string(params.Where[i].Type))
+			paramValues.Add("where[%d][attribute]=%s", params.Where[i].Attribute)
+			paramValues.Add("where[%d][value]=%s", params.Where[i].Value)
+		}
+	}
+
+	client.url.RawQuery = paramValues.Encode()
+
+	request, err := NewRequest(client, method, client.url.String())
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := SendRequest(request)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, err
+}
+
+// Link relates two records as documented at https://docs.espocrm.com/development/api/relationships/#link.
+func (client *ApiClient) Link(entityType, entityId, relatedEntityType string, relatedEntityIds []string) ([]byte, error) {
+	method := "POST"
+	endpoint := fmt.Sprintf("/%s/%s/%s", entityType, entityId, relatedEntityType)
+
+	payload := struct {
+		Ids []string `json:"ids"`
+	}{
+		Ids: relatedEntityIds,
+	}
+
+	jsonPayload, err := json.Marshal(payload)
+	if err != nil {
+		return nil, err
+	}
+
+	request, err := NewRequestWithBody(client, method, endpoint, jsonPayload)
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := SendRequest(request)
+	if err != nil {
+		return nil, err
+	}
+
+	return response, err
+}
+
+// Unlink unrelates two records as documented at https://docs.espocrm.com/development/api/relationships/#unlink.
+func (client *ApiClient) Unlink(entityType, entityId, relatedEntityType string, relatedEntityIds []string) ([]byte, error) {
+	method := "DELETE"
+	endpoint := fmt.Sprintf("/%s/%s/%s", entityType, entityId, relatedEntityType)
+
+	payload := struct {
+		Ids []string `json:"ids"`
+	}{
+		Ids: relatedEntityIds,
+	}
+
+	jsonPayload, err := json.Marshal(payload)
+	if err != nil {
+		return nil, err
+	}
+
+	request, err := NewRequestWithBody(client, method, endpoint, jsonPayload)
 	if err != nil {
 		return nil, err
 	}
